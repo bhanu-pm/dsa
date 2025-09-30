bb98d81d8169d1252b7a09fc2bdcf70d60856b56
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-09-29 22:42:07 -0400

    Time: 1139 ms (13.54%) | Memory: 20.7 MB (70.59%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -12,6 +12,8 @@ class Solution:
             j = length - 1
 
             while i < j:
+                summ = element + nums[i] + nums[j]
+
                 if (i > idx + 1) and (nums[i] == nums[i-1]):
                     i += 1
                     continue
@@ -19,13 +21,16 @@ class Solution:
                 if (j < length - 1) and (nums[j] == nums[j+1]):
                     j -= 1
                     continue
-                
-                summ = element + nums[i] + nums[j]
 
                 if summ == 0:
                     output.append([element, nums[i], nums[j]])
                     i += 1
                     j -= 1
+
+                    while (i < j) and (nums[i] == nums[i-1]):
+                        i += 1
+                    while (i < j) and (nums[j] == nums[j+1]):
+                        j -= 1
                 elif summ < 0:
                     i += 1
                 else:
