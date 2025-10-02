83987404380b8e336967573b6a8c813aa08c6439
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-10-02 19:57:39 -0400

    Time: 22 ms (52.88%) | Memory: 14.1 MB (17.07%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.go
+++ 217-contains-duplicate/contains-duplicate.go
@@ -2,8 +2,7 @@ func containsDuplicate(nums []int) bool {
     seen := make(map[int]int)
 
     for _, num := range nums{
-        _, exists := seen[num]
-        if exists{
+        if _, exists := seen[num]; exists{
             return true
         }
         seen[num] = 1
