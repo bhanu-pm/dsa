4525b10a1c15642a33c5ea12c20df0635f266da6
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-10-03 23:14:35 -0400

    Time: 1861 ms (5.05%) | Memory: 9.5 MB (71.19%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -1,3 +1,5 @@
+import "sort"
+
 func groupAnagrams(strs []string) [][]string {
     result := [][]string{}
     if len(strs) < 2 {
@@ -5,45 +7,40 @@ func groupAnagrams(strs []string) [][]string {
         return result
     }
 
-    seen := make(map[string]map[rune]int)
-
+    // sorting methodu
+    sortedStrs := make(map[string]string)
     for _, str := range strs {
-        seen[str] = make(map[rune]int)
-        for _, letter := range str {
-            seen[str][letter]++
-        }
+        runes := []rune(str)
+        sort.Slice(runes, func(i, j int) bool {
+            return runes[i] < runes[j]
+        })
+        sorted := string(runes)
+        sortedStrs[str] = sorted
     }
+
     doneIdx := make(map[int]bool)
     for i:=0; i<len(strs); i++ {
         if doneIdx[i] {
             continue
         }
+
         str := strs[i]
         doneIdx[i] = true
         group := []string{}
         group = append(group, str)
-        
-        str2Loop:
+
         for j:=0; j<len(strs); j++ {
-            str2 := strs[j]
-            // if doneIdx[j]{
-            //     continue
-            // }
+            if doneIdx[j] {
+                continue
+            }
             if i == j {
                 continue
             }
+            str2 := strs[j]
             if len(str) != len(str2) {
                 continue
             }
-            if str == str2 {
-                group = append(group, str2)
-                doneIdx[j] = true
-            } else {
-                for _, letter := range str {
-                    if seen[str][letter] != seen[str2][letter] {
-                        continue str2Loop
-                    }
-                }
+            if sortedStrs[str] == sortedStrs[str2] {
                 group = append(group, str2)
                 doneIdx[j] = true
             }
