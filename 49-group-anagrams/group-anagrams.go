f2b96297eb1a96571e7c7b2ed7420dd912aad499
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-10-04 03:06:50 -0400

    Time: 9 ms (70.81%) | Memory: 10 MB (37.63%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -7,45 +7,18 @@ func groupAnagrams(strs []string) [][]string {
         return result
     }
 
-    // sorting methodu
-    sortedStrs := make(map[string]string)
+    sortedStrs := make(map[string][]string)
     for _, str := range strs {
         runes := []rune(str)
         sort.Slice(runes, func(i, j int) bool {
             return runes[i] < runes[j]
         })
         sorted := string(runes)
-        sortedStrs[str] = sorted
+        sortedStrs[sorted] = append(sortedStrs[sorted], str)
     }
 
-    doneIdx := make(map[int]bool)
-    for i:=0; i<len(strs); i++ {
-        if doneIdx[i] {
-            continue
-        }
-
-        str := strs[i]
-        doneIdx[i] = true
-        group := []string{}
-        group = append(group, str)
-
-        for j:=0; j<len(strs); j++ {
-            // if doneIdx[j] {
-            //     continue
-            // }
-            if i == j {
-                continue
-            }
-            str2 := strs[j]
-            if len(str) != len(str2) {
-                continue
-            }
-            if sortedStrs[str] == sortedStrs[str2] {
-                group = append(group, str2)
-                doneIdx[j] = true
-            }
-        }
-        result = append(result, group)
+    for _, value := range sortedStrs {
+        result = append(result, value)
     }
     return result
 }
