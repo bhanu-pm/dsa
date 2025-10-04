e28bc408742c96932dbf4a26fc66d346048bb399
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-10-04 03:49:51 -0400

    Time: 13 ms (39.70%) | Memory: 10.7 MB (20.94%) - LeetSync

--- 49-group-anagrams/group-anagrams.go
+++ 49-group-anagrams/group-anagrams.go
@@ -1,10 +1,4 @@
 func groupAnagrams(strs []string) [][]string {
-    result := [][]string{}
-    if len(strs) < 2 {
-        result = append(result, strs)
-        return result
-    }
-
     sortedStrs := make(map[[26]int][]string)
     for _, str := range strs {
         key := [26]int{}
@@ -14,6 +8,7 @@ func groupAnagrams(strs []string) [][]string {
         sortedStrs[key] = append(sortedStrs[key], str)
     }
 
+    result := make([][]string, 0, len(sortedStrs))
     for _, value := range sortedStrs {
         result = append(result, value)
     }
