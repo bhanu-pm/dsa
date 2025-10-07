a100f1e281ef0a62c7411f19645210b3d09b8aeb
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-10-07 05:23:25 -0400

    Time: 0 ms (100.00%) | Memory: 7.8 MB (80.11%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.go
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -2,28 +2,27 @@ func trap(height []int) int {
     if len(height) < 3 {
         return 0
     }
-
-    total := 0
     i := 0
     j := len(height) - 1
     leftmax := 0
     rightmax := 0
+    total := 0
 
-    for i<j {
-        if height[i] > height[j] {
-            if height[j] >= rightmax {
-                rightmax = height[j]
-            } else {
-                total += rightmax - height[j]
-            }
-            j--
-        } else {
-            if height[i] >= leftmax {
-                leftmax = height[i]
-            } else {
+    for i < j {
+        if height[i] < height[j] {
+            if height[i] < leftmax {
                 total += leftmax - height[i]
+            } else {
+                leftmax = height[i]
             }
             i++
+        } else {
+            if height[j] < rightmax {
+                total += rightmax - height[j]
+            } else {
+                rightmax = height[j]
+            }
+            j--
         }
     }
     return total
