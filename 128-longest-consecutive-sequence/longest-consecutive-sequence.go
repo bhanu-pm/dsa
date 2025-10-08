ed1e4dc7f65f1599b88e7a1e653e7d79eae98d5b
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-10-08 19:45:51 -0400

    Time: 53 ms (50.19%) | Memory: 14 MB (18.71%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.go
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.go
@@ -1,12 +1,13 @@
 func longestConsecutive(nums []int) int {
     dict := make(map[int]bool)
+    engine := make([]int, 0, len(nums))
+    longest := 0
+    sequence := 0
 
     for _, i := range nums {
         dict[i] = true
     }
-
-    longest := 0
-    engine := make([]int, 0, len(nums))
+    
     for key, _ := range dict {
         if _, ok := dict[key-1]; !ok {
             engine = append(engine, key)
@@ -14,7 +15,7 @@ func longestConsecutive(nums []int) int {
     }
 
     for _, eng := range engine {
-        sequence := 1
+        sequence = 1
         for i:=1; i<=len(nums); i++ {
             if _, ok := dict[eng+i]; ok {
                 sequence++
