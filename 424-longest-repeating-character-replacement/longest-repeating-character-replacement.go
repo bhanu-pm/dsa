3ecc90a31a5226189293e64ba03ed91fd54be5df
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-10-24 17:34:02 -0400

    Time: 7 ms (73.77%) | Memory: 5.4 MB (66.98%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.go
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.go
@@ -2,18 +2,16 @@ func characterReplacement(s string, k int) int {
     longest := 0
     left := 0
     maxJ := 0
-    counts := make(map[byte]int)
+    counts := make(map[rune]int)
 
     for right, letter := range s {
-        counts[byte(letter)]++
-        for _, j := range counts {
-            if j > maxJ {
-                maxJ = j
-            }
+        counts[letter]++
+        if counts[letter] > maxJ {
+            maxJ = counts[letter]
         }
         len := right - left + 1
         if (len - maxJ) > k {
-            counts[s[left]]--
+            counts[rune(s[left])]--
             left++
         } else if len > longest {
             longest = len
