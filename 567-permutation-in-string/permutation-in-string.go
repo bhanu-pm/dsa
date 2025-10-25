a88d997dd4fa3cb4b717cfdfc16030cc68ffd2b4
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-10-24 20:26:42 -0400

    Time: 1601 ms (5.12%) | Memory: 69.8 MB (5.80%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.go
+++ 567-permutation-in-string/permutation-in-string.go
@@ -1,6 +1,5 @@
 func checkInclusion(s1 string, s2 string) bool {
     dict := make(map[byte]int, len(s1))
-    dict2 := make(map[byte]int, len(s2))
     i := 0
     j := len(s1)
     for a, _ := range s1 {
@@ -8,7 +7,7 @@ func checkInclusion(s1 string, s2 string) bool {
     }
     for j<=len(s2) {
         if _, ok := dict[s2[i]]; ok {
-            clear(dict2)
+            dict2 := make(map[byte]int, len(s2))
             for b, _ := range s2[i:j] {
                 dict2[s2[i+b]]++
             }
