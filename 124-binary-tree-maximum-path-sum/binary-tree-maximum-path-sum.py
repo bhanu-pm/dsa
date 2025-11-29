3b0914206f6911847e76c18860c261bdf95785d2
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-11-28 21:46:45 -0500

    Time: 21 ms (23.89%) | Memory: 23.1 MB (41.13%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -15,26 +15,32 @@ class Solution:
             left = dfs(root.left)
             right = dfs(root.right)
 
+            # no children nodes
             if root.left is None and root.right is None:
                 self.best = max(self.best, root.val)
                 return root.val
-
+            
+            # left child node
             if root.right is None:
                 leftbranch = max(root.val, root.val + left)
                 self.best = max(self.best, leftbranch)
                 return leftbranch
+            
+            # right child node
             if root.left is None:
                 rightbranch = max(root.val, root.val + right)
                 self.best = max(self.best, rightbranch)
                 return rightbranch
             
-            besthere = max(root.val, 
-                           root.val + left, 
-                           root.val + right, 
-                           root.val + left + right, 
-                           left, 
-                           right)
-            self.best = max(self.best, besthere)
+            best_path_until_now = max(root.val,
+                                      root.val + left,
+                                      root.val + right,
+                                      root.val + left + right,
+                                      left,
+                                      right)
+            
+            self.best = max(self.best, best_path_until_now)
             return max(root.val, root.val + left, root.val + right)
+        
         dfs(root)
         return self.best
