61bf67b77d453a382b16844b88a3f05e50eb5679
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-11-29 15:47:47 -0500

    Time: 32 ms (90.35%) | Memory: 17.9 MB (22.55%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,13 @@
 class Solution:
     def __init__(self):
-        self.mem = [0, 1]
+        self.mem = [0, 1, 1]
         self.len = 2
     def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+
         if n < self.len:
             return self.mem[n]
         
