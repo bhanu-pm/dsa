7dfef89f628aa36a9f0019f0daea4f1e5e06c9df
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-11-29 18:51:29 -0500

    Time: 0 ms (100.00%) | Memory: 19 MB (24.00%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,12 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # key: complement -> value: index
-        hash = {}
+        compl = {}
+        # key remaining, val index
         for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hash:
-                return [i, hash[complement]]
-            
-            # key: current num -> value: i
-            hash[nums[i]] = i
+            remaining = target - nums[i]
+            if remaining in compl:
+                return [i, compl[remaining]]
+            compl[nums[i]] = i
+        
         return [-1, -1]
