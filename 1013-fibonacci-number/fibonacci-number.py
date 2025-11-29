3ea8743bb876f82e7889c9a30c192e4a8e703220
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-11-29 16:00:25 -0500

    Time: 36 ms (76.78%) | Memory: 17.8 MB (22.55%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -3,15 +3,28 @@ class Solution:
         self.mem = [0, 1, 1]
         self.len = 2
     def fib(self, n: int) -> int:
+        # # Recursive sol
+        # if n == 0:
+        #     return 0
+        # if n == 1 or n == 2:
+        #     return 1
+
+        # if n < self.len:
+        #     return self.mem[n]
+        
+        # fn = self.fib(n-1) + self.fib(n-2)
+        # self.mem.append(fn)
+        # self.len += 1
+        # return fn
+
+        # Bottom up sol
         if n == 0:
             return 0
         if n == 1 or n == 2:
             return 1
-
-        if n < self.len:
-            return self.mem[n]
         
-        fn = self.fib(n-1) + self.fib(n-2)
-        self.mem.append(fn)
-        self.len += 1
-        return fn
+        for i in range(3, n+1):
+            fi = self.mem[i-1] + self.mem[i-2]
+            self.mem.append(fi)
+            # self.len += 1
+        return self.mem[n]
