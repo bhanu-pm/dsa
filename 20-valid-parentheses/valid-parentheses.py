e2b6959daf57fa0d294e2c16971344d283534d87
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-12-02 18:46:34 -0500

    Time: 4 ms (13.10%) | Memory: 17.7 MB (86.23%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,25 @@
+#  - Open brackets should be closed by the same type.
+#  - correct order
+#  - every open bracket has a corresponding close brack and vice versa.
+
+from collections import deque
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closed = {"}":"{", ")":"(", "]":"["}
+        stack = deque()
+        for i in s:
+            if i in closed: # closed bracket
+                if stack:
+                    openb = stack.pop()
+                    if closed[i] == openb:
+                        continue
+                    else:
+                        return False
+                else:
+                    return False
+            else: # open bracket
+                stack.append(i)
+        if stack:
+            return False
+        return True
+        
