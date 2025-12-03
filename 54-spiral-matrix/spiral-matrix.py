981a494ffe349a4551032fdc495880806b3cf9eb
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2025-12-02 20:12:59 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (18.18%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -14,10 +14,8 @@ class Solution:
             if len(output) == total_len:
                 break
 
-            # continue doing our thing
             if direction % 4 == 0:
-                # moving right
-                # only increment j
+                # moving right, only increment j
                 output.append(matrix[i][j])
                 seen.add((i, j))
                 if j+1 < len(matrix[0]) and ((i, j+1) not in seen):
@@ -26,8 +24,7 @@ class Solution:
                     direction += 1
                     i+=1
             elif direction % 4 == 1:
-                # moving down
-                # only increment i
+                # moving down, only increment i
                 output.append(matrix[i][j])
                 seen.add((i, j))
                 if i+1 < len(matrix) and ((i+1, j) not in seen):
@@ -36,22 +33,18 @@ class Solution:
                     direction += 1
                     j-=1
             elif direction % 4 == 2:
-                # moving left
-                # only decrement j
+                # moving left, only decrement j
                 output.append(matrix[i][j])
                 seen.add((i, j))
-                next_i, next_j = i, j-1
                 if j-1 >= 0 and ((i, j-1) not in seen):
                     j-=1
                 else:
                     direction += 1
                     i-=1
             elif direction %4 == 3:
-                # moving up
-                # only decrement i
+                # moving up, only decrement i
                 output.append(matrix[i][j])
                 seen.add((i, j))
-                next_i, next_j = i-1, j
                 if i-1 >= 0 and ((i-1, j) not in seen):
                     i-=1
                 else:
