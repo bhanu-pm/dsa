c60676b11c5a62550102c1271f32eaa971db6834
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-01-17 16:28:55 -0500

    Time: 93 ms (43.07%) | Memory: 28.9 MB (6.14%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -8,7 +8,7 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_diff = 0
         current_min = float('inf')
-        current_max = float('-inf')
+        # current_max = float('-inf')
         buy = 0
         sell = 0
         while sell < len(prices):
@@ -16,8 +16,8 @@ class Solution:
                 current_min = prices[sell]
                 buy = sell
                 # current_max = float('-inf')
-            elif prices[sell] > current_max:
-                current_max = prices[sell]
+            # elif prices[sell] > current_max:
+            #     current_max = prices[sell]
             max_diff = max(max_diff, prices[sell]-prices[buy])
             sell+=1
         return max_diff
