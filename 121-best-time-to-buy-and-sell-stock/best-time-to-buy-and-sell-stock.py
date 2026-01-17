d7277f8950793bfbd19fffdee71e1e35c534839b
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-01-17 16:08:44 -0500

    Time: 50 ms (72.31%) | Memory: 28.9 MB (6.14%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,23 @@
+# two pointers buy and sell
+    # simply buy = lowest seen so far
+    # sell = highest seen after buy
+# return diff
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        mini = 9999
-        output = 0
-        for today in prices:
-            diff = today - mini
-
-            if diff > output:
-                output = diff
-
-            if mini > today:
-                mini = today
-        
-        return output
+        max_diff = 0
+        current_min = float('inf')
+        current_max = float('-inf')
+        buy = 0
+        sell = 0
+        while sell < len(prices):
+            if prices[sell] < current_min:
+                current_min = prices[sell]
+                buy = sell
+                current_max = float('-inf')
+            elif prices[sell] > current_max:
+                current_max = prices[sell]
+                max_diff = max(max_diff, prices[sell]-prices[buy])
+            sell+=1
+        return max_diff
