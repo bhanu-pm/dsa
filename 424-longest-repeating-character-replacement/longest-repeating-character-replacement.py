98e8179ad77514d38d6492b2901459e150fbb07f
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-01-26 14:38:07 -0500

    Time: 232 ms (12.78%) | Memory: 19.8 MB (20.15%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,53 +1,39 @@
-# dynamic sliding window
-# arr to maintain counts of all letters
-# loop through count arr to find most frequent letter
-# track max_len of substring
-# window is valid, if, window size - most freq char count <= k
-    # i.e total stray char count <= k
+# 26 possible english uppercase characters
+# no. of switches <= k
+# need the longest valid substring's length
+# what is valid? -> substring length - most occurences of single character <= k
 
-# while right < len(s):
-    # if window valid
-        # check and update max_len accordingly
-        # increment right pointer
-        # increment count of char at right pointer
-    # else
-        # decrease count of left pointer char
-        # increment left pointer
+# dynamic subwindow
+# need a dict of 26 Uppercase english letters, to track counts, count
+# track max_len
+
+# for right loop
+    # count[right] +1
+
+    # while substr len - max_count > k: (non-validity condition)
+        # count[left] -1
+        # left += 1
+    
+    # max_len = max (maxlen, current len)
 # return max_len
 
-# for right in range len(s):
-    # increment right char count
-    # while invalid
-        # shrink
-        # s[left] -= 1
-        # left += 1
-    # update max_len
-    
-
-# one edge case is when 
+# time complex, O(26*N), 26 to find max_count in the count dict
+# O(1) space complexity, cause count dict fixed size, other variables
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        count = {key:0 for key in letters}
         max_len = float('-inf')
+        alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        count = {key:0 for key in alph}
         left = 0
-        right = 0
 
-        len_s = len(s)
-        for right in range(len_s):
+        for right in range(len(s)):
             count[s[right]] += 1
-            # while invalid shrink
-            while (right-left+1-self._max(count)) > k:
+
+            while (right-left+1 - max(count.values())) > k:
                 count[s[left]] -= 1
                 left += 1
+            
             max_len = max(max_len, right-left+1)
-                
+        
         return max_len
-
-    def _max(self, count):
-        max_count = 0
-        for i in count.keys():
-            if count[i] > max_count:
-                max_count = count[i]
-        return max_count
