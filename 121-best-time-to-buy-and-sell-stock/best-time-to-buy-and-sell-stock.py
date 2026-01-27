648fceb274c9d0a1b99263765dee6d2b15d47ad5
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-01-26 22:00:23 -0500

    Time: 59 ms (61.38%) | Memory: 28.8 MB (16.57%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,22 +1,27 @@
-# two pointers, dynamic
-# track max price diff
-# track least price val
-# loop
-    # check if the current pointer val is less than least price
-        # if yes, this is the new buy
-        # update least price
-    # calc, price diff and update the max price diff accordingly
-# return max diff
+# given arr
+# dynamic subwindow
+# don't care about index in the output
+
+# track max_diff = 0 , since we return 0 if no soln.
+# track min_val
+# buy = 0
+# for sell in range()
+    # if val < min_val
+        # min_val = val
+        # this is the new buy val
+    # max_diff = max(max_diff, diff)
+# return maxdiff
+
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        max_diff = 0
+        min_val = float('inf')
         buy = 0
-        least_price = float('inf')
-        max_diff = float('-inf')
 
         for sell in range(len(prices)):
-            if prices[sell] < least_price:
-                least_price = prices[sell]
+            if prices[sell] < min_val:
+                min_val = prices[sell]
                 buy = sell
             max_diff = max(max_diff, prices[sell]-prices[buy])
         
