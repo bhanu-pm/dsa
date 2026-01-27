9ca4110b1466542d744138a6129fb482f83e780e
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-01-27 16:21:22 -0500

    Time: 123 ms (30.06%) | Memory: 34.6 MB (13.77%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,19 +1,6 @@
-# input arr of temps
-# if no soln. answer[i] = 0
-# so init arr with 0s
+# monotonically decreasing stack
+# 
 
-# monotonically decreasing stack first
-
-# if no element in stack
-    # push into stack
-# else
-    # while stack and curr > stack[-1]
-        # popped = stack pop()
-        # it means we found the next big element
-        # output[poppped[1]] = curr_idx - popped[1]
-    # else
-        # push into stack
-# return output
 
 from collections import deque
 class Solution:
@@ -22,12 +9,11 @@ class Solution:
         output = [0] * len(temperatures)
 
         for i in range(len(temperatures)):
-            if not stack:
-                stack.append((temperatures[i], i))
-            else:
+            if stack:
                 while stack and temperatures[i] > stack[-1][0]:
                     a, b = stack.pop()
                     output[b] = i-b
-                else:
-                    stack.append((temperatures[i], i))
+
+            stack.append((temperatures[i], i))
+
         return output
