b58b19f33ff6c36a0e56af043f2a245517d61867
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-01-28 17:23:25 -0500

    Time: 3 ms (80.60%) | Memory: 20.6 MB (31.24%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,13 +1,28 @@
+# monotonically increasing order input array (<=)
+# two numbers should add up to target sum
+# don't use same element twice
+
+# two pointers, starting at each end
+# if sum(left, right) == target
+    # return left, right
+# elif sum < target
+    # so we have to increase the sum
+    # so, we can do left++, (monotonically increasing numbers)
+# elif sum > target
+    # so decrease sum
+    # right --
+# return left, right
+
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        i = 0
-        j = len(numbers) - 1
-        for num in numbers:
-            total = numbers[i] + numbers[j]
-            if total == target:
-                return [i+1, j+1]
-            
-            if total < target:
-                i += 1
+        left = 0
+        right = len(numbers)-1
+
+        while left < right:
+            if numbers[left] + numbers[right] > target:
+                right -= 1
+            elif numbers[left] + numbers[right] < target:
+                left += 1
             else:
-                j -= 1
+                return [left+1, right+1]
