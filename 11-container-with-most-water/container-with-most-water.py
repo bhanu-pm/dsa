10310f2570e2dcb5c3433ba2ac416dfebe98d67b
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-02 17:06:04 -0500

    Time: 53 ms (86.39%) | Memory: 29.6 MB (42.55%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,21 +1,44 @@
+# input int array of heights
+# find max area possible from the given heights
+# not sortable
+# area = min(both heights) * (right-left pointer)
+
+# track a max_area
+# while left < right:
+    # calc area
+    # max_area = max(max_area, area)
+    # if heightleft < heightright
+        # left++
+    # elif >
+        # right--
+    # else ==
+        # left++
+        # right--
+# return maxarea
+
+# edge case where less than 2 heights given
+# return 0
+
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        i = 0
-        j = len(height) - 1
-        max_area = 0
-        while i < j:
-            i_height = height[i]
-            j_height = height[j]
-            area = (j-i) * min([i_height, j_height])
-            if area > max_area:
-                max_area = area
-            
-            if i_height < j_height:
-                i += 1
-            elif i_height > j_height:
-                j -= 1
+        if len(height) < 2:
+            return 0
+        
+        max_area = float('-inf')
+        left = 0
+        right = len(height)-1
+
+        while left < right:
+            area = min(height[left], height[right]) * (right-left)
+            max_area = max(max_area, area)
+
+            if height[left] > height[right]:
+                right-=1
+            elif height[left] < height[right]:
+                left+=1
             else:
-                i += 1
-                j -= 1
+                left+=1
+                right-=1
         
         return max_area
