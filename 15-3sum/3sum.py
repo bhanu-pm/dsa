35b9f7d3c56111d7e647c874798ed35778d90008
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-02 16:53:11 -0500

    Time: 582 ms (63.19%) | Memory: 22 MB (49.23%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,77 +1,47 @@
-# values from 3 different indexes
-# sum of 3 values = 0, i+j+k=0, j+k=-i
-# how do I make sure the sol sets are different?????????????????????????????
-# maybe we can maintain a set of all sols
-    # and calc all possible sols
-# they are not asking for indexes, but only values,
-# so sorting is allowed
+# i, j, k shouldn't be equal
+# sum = 0, so, numj+numk = -numi
+# 10**5, so should be less than O(n**2)
+# input array, output list(list(int))
+# we don't need the exact indices, but only the values, so SORTING ALLOWED
 
-# sort the input
+# there might be duplicates, so we sort the triplets themselves and store in an output set.
+# sort input arr
 # for i
-    # init j=0, k=len(nums)-1
-    # while j<k
-        # standard two pointer sol
-        # with target sum (j+k) = -i
-        # if j or k = i
-            # skip, j+= 1 or k -= 1
-    # store all possible soln.s in set
-# turn set to list and return
+    # skip i values that are same
+    # while j<k:
+        # if < target
+            # j++
+        # elif > target
+            # k--
+        # else
+            # output.add(sort(triplet))
+# return List(output)
+
+# time complex < O(N**2)
+
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        output = set()
         nums.sort()
-        for i, num in enumerate(nums):
-            if i>0 and nums[i-1] == num:
+        output = set()
+        l = len(nums)-1
+
+        for idx, num in enumerate(nums):
+            if idx > 0 and num == nums[idx-1]:
                 continue
-            target = -nums[i]
-            j=i+1
-            k=len(nums)-1
-            while j<k:
-                if nums[j] + nums[k] < target:
-                    j+=1
-                elif nums[j] + nums[k] > target:
-                    k-=1
+            left=idx+1
+            right = l
+            target = -num
+            while left < right:
+                if nums[left] + nums[right] < target:
+                    left+=1
+                elif nums[left] + nums[right] > target:
+                    right-=1
                 else:
-                    output.add((nums[i], nums[j], nums[k]))
-                    j+=1
-                    k-=1
+                    triplet = [num, nums[left], nums[right]]
+                    triplet.sort()
+                    output.add(tuple(triplet))
+                    left+=1
+                    right-=1
+        
         return list(output)
-
-
-#####################################################################################################
-# alternate sol.
-# use the unsorted 2sum soln, using a hashmap
-# output_set
-# for i
-    # dict()
-    # target = -i
-    # for j
-        # if j in dict
-            # 
-        # new_target = target-j
-        # if new_target in dict:
-            # output_set.add([i, j, new_target])
-        # else:
-            # dict[new_target] = j
-    # return list(outputset)
-
-# class Solution:
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         output_set = set()
-#         seen_i = set()
-#         for i in range(len(nums)):
-#             if nums[i] not in seen_i:
-#                 target = -nums[i]
-#                 compl_set = set()
-
-#                 for j in range(i+1, len(nums)):
-#                     # j+k=-i, k=-i-j
-#                     complement = target-nums[j]
-#                     if complement in compl_set:
-#                         tup = [nums[i], nums[j], complement]
-#                         tup.sort()
-#                         output_set.add(tuple(tup))
-#                     else:
-#                         compl_set.add(nums[j])
-#         return list(output_set)
