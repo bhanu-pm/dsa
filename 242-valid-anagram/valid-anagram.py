fcbefacaee97724ee6f591eae6bdb0f66258f0a0
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-07 17:25:21 -0500

    Time: 15 ms (42.71%) | Memory: 19.6 MB (25.77%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,35 @@
+# s and t strings given
+# if s and t are anagrams
+    # return true
+# return false
+
+# two hashmaps, for s and t
+# for i in s
+    # add to hashmap
+# for i in t
+    # add to hashmap
+# if s==t
+    # return true
+# return false
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        smap = dict()
+        tmap = dict()
+
+        for i in s:
+            if i in smap:
+                smap[i] += 1
+            else:
+                smap[i] = 1
+        
+        for i in t:
+            if i in tmap:
+                tmap[i] += 1
+            else:
+                tmap[i] = 1
+        
+        if smap == tmap:
+            return True
+        return False
