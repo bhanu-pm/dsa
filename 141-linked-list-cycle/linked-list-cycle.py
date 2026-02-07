ecbfa6ba02fd101d5b2c117b8275ac97fcfcafe9
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-07 16:44:48 -0500

    Time: 44 ms (86.54%) | Memory: 22.8 MB (8.60%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -0,0 +1,37 @@
+# use a set to store values
+# if value in set, we got a cycle?
+
+# using two pointers
+# left = head
+# right = head.next
+# while True:
+    # if left == right.next:
+        # return True
+    # if 
+    # right = right.next
+    # 
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        left = head
+        right = head.next
+        while right:
+            if right.next == left or right == left:
+                return True
+            left = left.next
+            right = right.next
+            if right:
+                right = right.next
+            else:
+                break
+        return False
