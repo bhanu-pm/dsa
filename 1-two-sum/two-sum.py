366a3c7f35a5f9f25b4a7475947a5f81f054a5fb
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-07 17:34:43 -0500

    Time: 0 ms (100.00%) | Memory: 20.5 MB (34.92%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,21 @@
+# return [i, j]
+
+# for i in range(nums:
+    # complement = target-i
+    # if complement in comp_dict:
+        # return [i, comp_dict[complement]]
+    # else:
+        # comp_dict[nums[i]] = i
+# return [-1, -1]
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        compl = {}
-        # key remaining, val index
-        for i in range(len(nums)):
-            remaining = target - nums[i]
-            if remaining in compl:
-                return [i, compl[remaining]]
-            compl[nums[i]] = i
-        
+        val_dict = dict()
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in val_dict:
+                return [i, val_dict[complement]]
+            else:
+                val_dict[num] = i
         return [-1, -1]
