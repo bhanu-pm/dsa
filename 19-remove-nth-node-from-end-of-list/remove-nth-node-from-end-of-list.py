8502c3e64fab123c27f974254a3fac7bd46e5356
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-10 15:47:44 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (17.07%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -18,33 +18,69 @@
 # return head
 
 
+################################################################
+
+# 1 pass soln.
+# we can use two pointers that are offset by n+1
+# [1 2 3 4 5], n=2, we remove '4'
+# [1 2 3 4 5]
+#      l   r
+# if right.next is None:
+    # if left==head:
+        # head = head.next
+    # else:
+        # left.next = right
+
 
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
-#         self.next = next
+# #         self.next = next
+# class Solution:
+#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+#         length = 0
+#         top = head
+#         while head:
+#             head = head.next
+#             length+=1
+        
+#         idx = length-n
+#         i = 0
+#         head = top
+#         temp = None
+#         while i<idx:
+#             temp = head
+#             head = head.next
+#             i+=1
+#         else:
+#             if temp:
+#                 head = head.next
+#                 temp.next = head
+#                 head = top
+#             else:
+#                 head = head.next
+#         return head
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        length = 0
-        top = head
-        while head:
-            head = head.next
-            length+=1
-        
-        idx = length-n
-        i = 0
-        head = top
         temp = None
-        while i<idx:
-            temp = head
-            head = head.next
-            i+=1
+        left = None
+        right = head
+        offset = 1
+        while right:
+            right = right.next
+            if offset == n:
+                if left:
+                    temp = left
+                    left = left.next
+                else:
+                    left = head
+            else:
+                offset+=1
         else:
             if temp:
-                head = head.next
-                temp.next = head
-                head = top
+                temp.next = left.next
             else:
                 head = head.next
+
         return head
