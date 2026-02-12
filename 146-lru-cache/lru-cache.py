9bbd76e33c2573fa1692297a685d82c08e051e83
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-12 14:57:34 -0500

    Time: 90 ms (94.10%) | Memory: 76.8 MB (87.91%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,18 +1,29 @@
-# Both get and put should run in O(1)
-    # So, Hashmap + dll
-        # hashmap points to the value in dll
-        # dll maintains the order of usage
-            # both get and put methods update the order of usage in DLL
+# least recently used would be evicted
+# we need to maintain an order, most recently used = at left, lru = at right
+# cache should stay within given capacity
+# get:
+    # return key if exist else -1
+# put:
+    # if key exist:
+        # update key
+    # else
+        # if capacity there;
+            # add key
+            # stored +=1
+        # else
+            # evict lru
+            # add key
+# for O(1) lookups, we need a dict
+# and this dict should have an order like doubly linked lists, for ease of access from both ends
+# and modifiability
 
-# Only gets return the value if item exists
-    # else, return -1
-    # put returns null
 
 from collections import OrderedDict
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
+        self.stored = 0
         self.cache = OrderedDict()
 
     def get(self, key: int) -> int:
@@ -22,15 +33,18 @@ class LRUCache:
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:                       # Updating existing cache
+        if key in self.cache:
             self.cache[key] = value
             self.cache.move_to_end(key, last=False)
-            return
-
-        if len(self.cache) == self.capacity:        # Writing new cache
-            self.cache.popitem(last=True)
-        self.cache[key] = value
-        self.cache.move_to_end(key, last=False)
+        else:
+            if self.stored < self.capacity:
+                self.cache[key] = value
+                self.cache.move_to_end(key, last=False)
+                self.stored += 1
+            else:
+                self.cache.popitem(last=True)
+                self.cache[key] = value
+                self.cache.move_to_end(key, last=False)
         return
 
 # Your LRUCache object will be instantiated and called as such:
