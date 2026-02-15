586ae59d7685c4e796fe8dff2b22ae4bde8320b9
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-15 10:52:26 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.15%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -11,48 +11,63 @@
                 # store root.right in stack
                     # process root.left
 
-from collections import deque
+# from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # if not p:
+        #     if not q:
+        #         return True
+        #     return False
+        # if not q:
+        #     if p:
+        #         return False
+        # stackp = deque()
+        # stackq = deque()
+        # stackp.append(p)
+        # stackq.append(q)
+        # while stackp and stackq:
+        #     p = stackp.pop()
+        #     q = stackq.pop()
+        #     if p is None and q is None:
+        #         continue
+        #     if p is None or q is None:
+        #         return False
+        #     if p.val != q.val:
+        #         return False
+        #     if not p.left and not p.right:
+        #         if not q.left and not q.right:
+        #             pass
+        #         else:
+        #             return False
+        #     if not q.left and not q.right:
+        #         if not p.left and not p.right:
+        #             pass
+        #         else:
+        #             return False
+
+        #     stackp.append(p.right)
+        #     stackq.append(q.right)
+        #     stackp.append(p.left)
+        #     stackq.append(q.left)
+        # return True
+
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p:
-            if not q:
-                return True
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
             return False
-        if not q:
-            if p:
-                return False
-        stackp = deque()
-        stackq = deque()
-        stackp.append(p)
-        stackq.append(q)
-        while stackp and stackq:
-            p = stackp.pop()
-            q = stackq.pop()
-            if p is None and q is None:
-                continue
-            if p is None or q is None:
-                return False
-            if p.val != q.val:
-                return False
-            if not p.left and not p.right:
-                if not q.left and not q.right:
-                    pass
-                else:
-                    return False
-            if not q.left and not q.right:
-                if not p.left and not p.right:
-                    pass
-                else:
-                    return False
-
-            stackp.append(p.right)
-            stackq.append(q.right)
-            stackp.append(p.left)
-            stackq.append(q.left)
-        return True
+        
+        if p.val == q.val:
+            if self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right):
+                return True
+        # else:
+        #     if self.isSameTree(p.left, q) or self.isSameTree(p.right, q):
+        #         return True
+        return False
