f366af691b9f68805ab01b91ee37abbc69bd878d
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-14 22:30:51 -0500

    Time: 877 ms (19.09%) | Memory: 22.5 MB (13.36%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,47 +1,38 @@
-# i, j, k shouldn't be equal
-# sum = 0, so, numj+numk = -numi
-# 10**5, so should be less than O(n**2)
-# input array, output list(list(int))
-# we don't need the exact indices, but only the values, so SORTING ALLOWED
+# given unsorted array of ints
+# return list of sorted triplets, no duplicate triplets
+# sorting should be OK
+# max time O(N*logN)
 
-# there might be duplicates, so we sort the triplets themselves and store in an output set.
-# sort input arr
+# result = set
 # for i
-    # skip i values that are same
-    # while j<k:
-        # if < target
-            # j++
-        # elif > target
-            # k--
-        # else
-            # output.add(sort(triplet))
-# return List(output)
+    # unsorted 2 sum of remaining
+    # sort the triplet
+        # add to result
+# return list (result)
 
-# time complex < O(N**2)
+######## OR
+# sort nums
+# for i
+    # sorted 2 sum of remaining
 
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        output = set()
-        l = len(nums)-1
+        result = set()
 
-        for idx, num in enumerate(nums):
-            if idx > 0 and num == nums[idx-1]:
+        for i, num in enumerate(nums):
+            if i>0 and num==nums[i-1]: # same i
                 continue
-            left=idx+1
-            right = l
+            complement = set()
             target = -num
-            while left < right:
-                if nums[left] + nums[right] < target:
-                    left+=1
-                elif nums[left] + nums[right] > target:
-                    right-=1
-                else:
-                    triplet = [num, nums[left], nums[right]]
+            for j in range(i+1, len(nums)):
+                compl = target-nums[j]
+                if compl in complement:
+                    triplet = [num, nums[j], compl]
                     triplet.sort()
-                    output.add(tuple(triplet))
-                    left+=1
-                    right-=1
+                    result.add(tuple(triplet))
+                else:
+                    complement.add(nums[j])
         
-        return list(output)
+        return list(result)
+                
