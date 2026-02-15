9b0bdc56c66c89029ff67bc8fa8ef5c8763d9509
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-14 22:41:33 -0500

    Time: 571 ms (69.71%) | Memory: 22 MB (66.93%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,6 +1,6 @@
 # given unsorted array of ints
 # return list of sorted triplets, no duplicate triplets
-# sorting should be OK
+# sorting allowed
 # max time O(N*logN)
 
 # result = set
@@ -19,20 +19,23 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         result = set()
+        nums.sort()
+        l = len(nums)
 
         for i, num in enumerate(nums):
-            if i>0 and num==nums[i-1]: # same i
+            if i>0 and num==nums[i-1]:
                 continue
-            complement = set()
             target = -num
-            for j in range(i+1, len(nums)):
-                compl = target-nums[j]
-                if compl in complement:
-                    triplet = [num, nums[j], compl]
-                    triplet.sort()
-                    result.add(tuple(triplet))
-                else:
-                    complement.add(nums[j])
-        
+            j = i+1
+            k = l-1
+            while j<k:
+                summ = nums[j] + nums[k]
+                if summ == target:
+                    result.add(tuple([num, nums[j], nums[k]]))
+                    j+=1
+                    k-=1
+                elif summ < target:
+                    j+=1
+                elif summ > target:
+                    k-=1
         return list(result)
-                
