f45330d56d3a3fcbd9ce60c579b937a01abac328
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-23 15:14:10 -0500

    Time: 9 ms (90.21%) | Memory: 22.3 MB (49.72%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,16 +1,36 @@
-#  - so anagrams are composed of same letters in same quantities
-#     - make each word a hashmap
-#     - check equality and append them 
-from collections import defaultdict
+# array of strings given
+# group all the anagrams in a list, return list(list(all groups))
+
+
+########## Naive solution
+# we can make a list of dictionaries
+    # each dict has counts of chars of a word
+# OrderedDict{word = dict{letter = count}}
+# final = []
+# while len(od.keys()) >= 0:
+    # od.keys()[0]
+    # group.append(word)
+    # del od[word]
+    # for newword in od.keys():
+        # if od[newword] == od[word]
+            # group.append(newword)
+            # del od[newword]
+    # final.append(group)
+# return final
+
+
+############ Group elements by property
+# dict(anagram = [list of words])
+# return list(dict.values())
+
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if len(strs) < 2:
-            return [strs]
-        
-        hashmap = defaultdict(list)
+        final = dict()
         for i in strs:
-            key = tuple(sorted(i))
-            hashmap[key].append(i)
-        return list(hashmap.values())
-        
+            keyl = sorted(i)
+            key = ''.join(keyl)
+            if key not in final:
+                final[key] = []
+            final[key].append(i)
+        return list(final.values())
