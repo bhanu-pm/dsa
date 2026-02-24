656031f4741d1befd8550e515e69ec0ba32e9e9c
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-24 14:19:23 -0500

    Time: 123 ms (25.15%) | Memory: 34.2 MB (25.25%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,15 +1,29 @@
+# array of temps
+# return answer arr
+
+# answer arr
+    # answer[i] = no. of days to wait after i'th day, to get a warmer temp.
+    # if no warmer future day, answer[i] = 0
+
+# monotonically decreasing stack
+    # this stack stores a value
+
+    # if we get a colder day
+        # we store it, index value
+    # while stack.top < new day
+        # we pop the stack
+        # j-i and store it in i'th index
+# return answer
 
 from collections import deque
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack = deque()
-        output = [0] * len(temperatures)
+        answer = [0] * len(temperatures)
+        for j, temperature in enumerate(temperatures):
+            while stack and stack[-1][1] < temperature:
+                i, s_temperature = stack.pop()
+                answer[i] = j-i
 
-        for i in range(len(temperatures)):
-            if stack:
-                while stack and temperatures[i] > stack[-1][0]:
-                    a, b = stack.pop()
-                    output[b] = i-b
-            stack.append((temperatures[i], i))
-
-        return output
+            stack.append((j, temperature))
+        return answer
