0a6a09256713be4a4625661a4c19776e7e60d07e
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-02-25 14:33:08 -0500

    Time: 86 ms (57.45%) | Memory: 28.9 MB (85.81%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -15,19 +15,11 @@
 import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        min_heap = []
-        heapq.heapify(min_heap)
-        for i in nums:
-            if len(min_heap) < k:
-                heapq.heappush(min_heap, i)
-            else:
-                heapq.heappush(min_heap, i)
-                heapq.heappop(min_heap)
-        return heapq.heappop(min_heap)
-
-        # popped = []
-        # heapq.heapify_max(nums)
-        # while len(popped) < k-1:
-        #     popped.append(heapq.heappop_max(nums))
-        # return heapq.heappop_max(nums)
+        popped = []
+        last = 0
+        heapq.heapify_max(nums)
+        while last < k-1:
+            popped.append(heapq.heappop_max(nums))
+            last += 1
+        return heapq.heappop_max(nums)
         
