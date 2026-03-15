48607d8235b04d982a558282c5fd481570cd597c
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-03-14 21:56:26 -0400

    Time: 16 ms (6.74%) | Memory: 19.5 MB (24.25%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,30 +1,18 @@
-# if value ! 4 or 9
-    # select max symbol < input
-    # input -= max symbol
+# ordered dict with all special values
 
-# if 4 or 9
-    # go to subtractive mode
-    # 4 = I - V
-    # 9 = I - X
+# while loop
+    # subtract biggest possible number in dict from input
 
-# only the following subtractive forms are used
-# 4, 9, 40, 90, 400, 900
-
-# powers of 10 (I, X, C, M) can be appended at max 3 times
-    # if 4 or 9, go to sub mode
-
-# 5, 50, 500 cannot be appended multiple times
-
-# num <= 3999
 
 class Solution:
     def intToRoman(self, num: int) -> str:
         result = []
-        int_roman = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]
+        od = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
 
-        for i in int_roman:
-            while num >= i[0]:
-                result.append(i[1])
-                num -= i[0]
-        
+        while num > 0:
+            for i in od.keys():
+                if num >= i:
+                    result.append(od[i])
+                    num -= i
+                    break
         return ''.join(result)
