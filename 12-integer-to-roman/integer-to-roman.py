44ba2481ae1343949c97a48f4a71a553ac24183f
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-03-14 22:00:51 -0400

    Time: 5 ms (50.97%) | Memory: 19.3 MB (86.67%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -9,8 +9,8 @@ class Solution:
         result = []
         od = [[1000,'M'], [900,'CM'], [500,'D'], [400,'CD'], [100,'C'], [90,'XC'], [50,'L'], [40,'XL'], [10,'X'], [9,'IX'], [5,'V'], [4,'IV'], [1,'I']]
 
-        for i, j in od:
-            while num >= i:
-                result.append(j)
-                num -= i
+        for i in od:
+            while num >= i[0]:
+                result.append(i[1])
+                num -= i[0]
         return ''.join(result)
