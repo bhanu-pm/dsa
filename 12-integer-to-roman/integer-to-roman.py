7688966b205291053d9bad1c5ee8826d7e66b96c
Bhanu Prakash Mopidevi <155854216+bhanu-pm@users.noreply.github.com>
2026-03-14 22:00:13 -0400

    Time: 8 ms (27.40%) | Memory: 19.4 MB (58.66%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -7,10 +7,10 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         result = []
-        od = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
+        od = [[1000,'M'], [900,'CM'], [500,'D'], [400,'CD'], [100,'C'], [90,'XC'], [50,'L'], [40,'XL'], [10,'X'], [9,'IX'], [5,'V'], [4,'IV'], [1,'I']]
 
-        for i in od.keys():
+        for i, j in od:
             while num >= i:
-                result.append(od[i])
+                result.append(j)
                 num -= i
         return ''.join(result)
